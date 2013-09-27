68c2569c9d254d1591363c4b057251e357d68a16
lucasdelevy <lucasdelevy@gmail.com>
2013-09-27 20:07:07 -0300

    First Real Commit

---
+++ UnB/src/com/example/hellogridview/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.hellogridview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+        
+        gridview.setOnItemClickListener(new OnItemClickListener()
+        {
+        	public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+        	{
+        		//View maps = new View(MainActivity.this);
+				Toast.makeText(MainActivity.this, "lalalala", Toast.LENGTH_SHORT).show();
+        		//openMaps(maps);
+        	}
+       	});
+    }
+    
+    public void openMaps(View view)
+    {
+    	Intent intent = new Intent (this, Maps.class);
+    	startActivity(intent);
+    }
+}
---
+++ UnB/src/com/example/hellogridview/Maps.java
@@ -0,0 +1,11 @@
+package com.example.hellogridview;
+
+import android.app.Activity;
+
+public class Maps extends Activity {
+	protected void onCreate()
+	{
+		super.onCreate(null);
+        setContentView(R.layout.maps);
+	}
+}
