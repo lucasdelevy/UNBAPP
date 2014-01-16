6461ce83e21af7d86ef63ed9e56549538d52ea21
Matheus Portela <matheus.v.portela@gmail.com>
2014-01-16 12:02:11 -0200

    Eventos com descrição
    Coloquei um espaço para a descrição dos eventos
    
    TODO:
    - Conseguir colocar as descrições
    - Pegar os nomes dos eventos e descrições de um arquivo XML

--- UnB/gen/com/example/hellogridview/R.java
+++ UnB/gen/com/example/hellogridview/R.java
@@ -29,13 +29,15 @@ public final class R {
         public static final int unb_map=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080008;
+        public static final int event_description=0x7f080005;
+        public static final int event_name=0x7f080004;
         public static final int events_button=0x7f080003;
         public static final int main_background=0x7f080000;
-        public static final int maps=0x7f080004;
+        public static final int maps=0x7f080006;
         public static final int maps_button=0x7f080001;
         public static final int schedule_button=0x7f080002;
-        public static final int unbmap=0x7f080005;
+        public static final int unbmap=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
