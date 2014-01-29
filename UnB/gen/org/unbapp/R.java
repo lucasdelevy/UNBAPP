9c02d8594499e1c3582be6f9fd657c14daa9dfe6
Matheus Portela <matheus.v.portela@gmail.com>
2014-01-29 20:25:44 -0200

    Pegando os eventos de um arquivo XML

--- UnB/gen/org/unbapp/R.java
+++ UnB/gen/org/unbapp/R.java
@@ -8,11 +8,14 @@
 package org.unbapp;
 
 public final class R {
+    public static final class array {
+        public static final int event_list=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int unb_blue_color=0x7f060001;
-        public static final int unb_green_color=0x7f060000;
+        public static final int unb_blue_color=0x7f070001;
+        public static final int unb_green_color=0x7f070000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -43,23 +46,23 @@ public final class R {
         public static final int unb_map=0x7f02000f;
     }
     public static final class id {
-        public static final int action_settings=0x7f090010;
-        public static final int emergency_button=0x7f090002;
-        public static final int emergency_contacts_list=0x7f090006;
-        public static final int emergency_subtitle_id=0x7f09000d;
-        public static final int emergency_title_id=0x7f09000c;
-        public static final int emergency_unb_address_id=0x7f090005;
-        public static final int emergency_unb_id=0x7f090004;
-        public static final int event_date_id=0x7f09000a;
-        public static final int event_image_id=0x7f090008;
-        public static final int event_location_id=0x7f09000b;
-        public static final int event_name_id=0x7f090009;
-        public static final int events_button=0x7f090003;
-        public static final int events_contacts_list=0x7f090007;
-        public static final int main_background=0x7f090000;
-        public static final int maps=0x7f09000e;
-        public static final int maps_button=0x7f090001;
-        public static final int unbmap=0x7f09000f;
+        public static final int action_settings=0x7f0a0010;
+        public static final int emergency_button=0x7f0a0002;
+        public static final int emergency_contacts_list=0x7f0a0006;
+        public static final int emergency_subtitle_id=0x7f0a000d;
+        public static final int emergency_title_id=0x7f0a000c;
+        public static final int emergency_unb_address_id=0x7f0a0005;
+        public static final int emergency_unb_id=0x7f0a0004;
+        public static final int event_date_id=0x7f0a000a;
+        public static final int event_image_id=0x7f0a0008;
+        public static final int event_location_id=0x7f0a000b;
+        public static final int event_name_id=0x7f0a0009;
+        public static final int events_button=0x7f0a0003;
+        public static final int events_contacts_list=0x7f0a0007;
+        public static final int main_background=0x7f0a0000;
+        public static final int maps=0x7f0a000e;
+        public static final int maps_button=0x7f0a0001;
+        public static final int unbmap=0x7f0a000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -70,19 +73,19 @@ public final class R {
         public static final int maps=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int bg_main=0x7f050002;
-        public static final int calendar_activity=0x7f050005;
-        public static final int emergency_activity=0x7f050007;
-        public static final int events_activity=0x7f050004;
-        public static final int maps_activity=0x7f050003;
-        public static final int unb=0x7f050009;
-        public static final int unb_address=0x7f050008;
-        public static final int unbmap=0x7f050006;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int bg_main=0x7f060002;
+        public static final int calendar_activity=0x7f060005;
+        public static final int emergency_activity=0x7f060007;
+        public static final int events_activity=0x7f060004;
+        public static final int maps_activity=0x7f060003;
+        public static final int unb=0x7f060009;
+        public static final int unb_address=0x7f060008;
+        public static final int unbmap=0x7f060006;
     }
     public static final class style {
         /** 
@@ -106,10 +109,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- UnB/src/org/unbapp/Events.java
+++ UnB/src/org/unbapp/Events.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -15,14 +17,22 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class Events extends ListActivity {
+	// Logging
+	private static final String TAG = "Events"; 
+	
+	// Types of events
+	private static final int EVENT_TYPE_HAPPY_HOUR = 0;
+	private static final int EVENT_TYPE_PARTY = 1;
+	private static final int EVENT_TYPE_GRADUATION = 2;
+	private static final int EVENT_TYPE_GENERAL_MEETING = 3;
 	
 	public class Event {
 		private String name;
-		private EventType type;
+		private int type;
 		private String location;
 		private String date;
 		
-		public Event(String name, EventType type, String location, String date) {
+		public Event(String name, int type, String location, String date) {
 			this.name = name;
 			this.type = type;
 			this.location = location;
@@ -33,7 +43,7 @@ public class Events extends ListActivity {
 			return name;
 		}
 		
-		public EventType getType() {
+		public int getType() {
 			return type;
 		}
 		
@@ -46,36 +56,6 @@ public class Events extends ListActivity {
 		}
 	}
 	
-	enum EventType {
-		HAPPY_HOUR, PALESTRA, FORMATURA, TCC, ASSEMBLEIA
-	}
-	
-	String[] events = new String[] {
-			"Happy Hour da Florestal",
-			"Formatura da Eng. Mecatr™nica",
-			"Assembleia Geral do DCE"
-	};
-	
-	EventType[] types = new EventType[] {
-			EventType.HAPPY_HOUR,
-			EventType.FORMATURA,
-			EventType.ASSEMBLEIA
-	};
-	
-	String[] locations = new String[] {
-			"FT",
-			"Centro Comunit‡rio",
-			"Ceubinho"
-	};
-	
-	String[] dates = new String[] {
-			"29/02 - 18:00",
-			"30/02 - 20:00",
-			"31/02 - 12:00"
-	};
-	
-	final int NUM_OF_EVENTS = events.length;
-	
 	private List<Event> myEvents = new ArrayList<Event>();
 	
 	@Override
@@ -85,15 +65,37 @@ public class Events extends ListActivity {
 		setTitle(R.string.events_activity);
 	    setContentView(R.layout.events);
 	    
-	    populateEventList();		// Fill myEvents
+	    getEventsFromXml();			// Fill myEvents
 	    populateListView();			// Fill the XML
 	    //registerClickCallback();	// Create listener
 	}
 	
-	private void populateEventList() {
-		for(int i = 0; i < NUM_OF_EVENTS; i++) {
-			myEvents.add(new Event(events[i], types[i], locations[i], dates[i]));
+	private void getEventsFromXml() {
+		Resources res = getResources();
+		String[] eventList = res.getStringArray(R.array.event_list);
+		String[] eventDetails;
+		int type;
+		String delimiter = "\\|";
+		
+		for (String event : eventList) {
+			eventDetails = event.split(delimiter);
+			type = processEventType(eventDetails[1]);
+			myEvents.add(new Event(eventDetails[0], type, eventDetails[2], eventDetails[3]));
 		}
+	}
+	
+	private int processEventType(String eventType) {
+		if(eventType.equals("Happy Hour"))
+			return EVENT_TYPE_HAPPY_HOUR;
+		else if(eventType.equals("Party"))
+			return EVENT_TYPE_PARTY;
+		else if(eventType.equals("Graduation"))
+			return EVENT_TYPE_GRADUATION;
+		else if(eventType.equals("General Meeting"))
+			return EVENT_TYPE_GENERAL_MEETING;
+		
+		Log.w(TAG, String.format("Unknown event type %s", eventType));
+		return -1;
 	}
 		
 	private void populateListView() {
@@ -127,14 +129,25 @@ public class Events extends ListActivity {
 			TextView location = (TextView) itemView.findViewById(R.id.event_location_id);
 			location.setText(currentEvent.getLocation());
 			
-			if (currentEvent.getType() == EventType.FORMATURA) {
-				View imageView = itemView.findViewById(R.id.event_image_id);
-				ImageView image = (ImageView) imageView;
-				Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.event_graduation);
-				image.setImageBitmap(bm);
-			}
+			View imageView = itemView.findViewById(R.id.event_image_id);
+			ImageView image = (ImageView) imageView;
+			int drawable = getEventTypeDrawable(currentEvent.getType());
+			Bitmap bm = BitmapFactory.decodeResource(getResources(), drawable);
+			image.setImageBitmap(bm);
 			
 			return itemView;
-		}	
+		}
+		
+		private int getEventTypeDrawable(int type) {
+			switch (type) {
+			case EVENT_TYPE_HAPPY_HOUR:
+				return R.drawable.ic_events_hh_new;
+			case EVENT_TYPE_PARTY:
+				return R.drawable.ic_events_festa_new;
+			default:
+				Log.w(TAG, String.format("No icon defined for event type %d", type));
+				return R.drawable.ic_events_new;
+			}
+		}
 	}
 }
